73edb958ed34624b2a480874cdbb940a1f34a074
Steve-Rion <swilson52@huskers.unl.edu>
2024-04-21 08:45:15 -0500

    Configure virt-column

--- config/ui/virt-column.nix
+++ config/ui/virt-column.nix
@@ -1,5 +1,29 @@
 {
   plugins.virt-column = {
     enable = true;
+    
+    settings = {
+      char = "┃";
+      enabled = true;
+      exclude = {
+          buftypes = [
+            "nofile"
+            "quickfix"
+            "terminal"
+            "prompt"
+          ];
+          filetypes = [
+            "lspinfo"
+            "packer"
+            "checkhealth"
+            "help"
+            "man"
+            "TelescopePrompt"
+            "TelescopeResults"
+          ];
+        };
+        highlight = "NonText";
+        virtcolumn = "";
+    };
   };
 }
