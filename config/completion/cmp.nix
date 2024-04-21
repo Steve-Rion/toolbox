9938229ea8c753d7698fd53bab72306f9ec5d2d6
Steve-Rion <swilson52@huskers.unl.edu>
2024-04-21 09:30:52 -0500

    remove luasnip completions

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -3,7 +3,6 @@
     cmp-nvim-lsp = {enable = true;}; # lsp
     cmp-buffer = {enable = true;};
     cmp-path = {enable = true;}; # file system paths
-    cmp_luasnip = {enable = true;}; # snippets
     cmp-cmdline = {enable = false;}; # autocomplete for cmdline
     cmp = {
       enable = true;
