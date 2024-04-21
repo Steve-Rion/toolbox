d5f646dfa530f4231293e152000cd6c1349c5c9f
Steve-Rion <swilson52@huskers.unl.edu>
2024-04-21 09:57:06 -0500

    Actually let's not

--- config/default.nix
+++ config/default.nix
@@ -47,7 +47,7 @@
     # ./ui/dressing-nvim.nix
     ./ui/indent-blankline.nix
     ./ui/noice.nix
-    ./ui/nvim-notify.nix
+    # ./ui/nvim-notify.nix
     ./ui/nui.nix
 
     # ./utils/better-escape.nix
